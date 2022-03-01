438e94a164cbdbeb1e7fe80246ab86982e7089c5
wjh791072385 <791072385@qq.com>
2022-03-01 15:27:33 +0800

    Cache数据结构初始化，实现LRU相关

---
+++ lru/lru.go
@@ -0,0 +1,81 @@
+package lru
+
+import "container/list"
+
+// Cache LRU
+type Cache struct {
+	maxBytes int64
+	nbytes   int64
+	ll       *list.List
+	cache    map[string]*list.Element
+
+	// optional and executed when an entry is purged.
+	// OnEvicted 是某条记录被移除时的回调函数，可以为 nil。
+	OnEvicted func(key string, value Value)
+}
+
+// entry 作为双向链表的结点类型
+type entry struct {
+	key   string //定义key string是为了方便删除结点时，删除Cache中的字典映射
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, onEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	ele, ok := c.cache[key]
+	if ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, ok
+	}
+	return nil, false
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele := c.cache[key]; ele != nil {
+		//如果存在则更新
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		//不存在则插入新节点
+		ele := c.ll.PushFront(&entry{key: key, value: value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+func (c *Cache) Len() int {
+	return c.ll.Len()
+}
