677301aa3e11216234eb538a8364257543aacdcc
wjh791072385 <791072385@qq.com>
2022-03-02 09:32:40 +0800

    一致性Hash基础

---
+++ consistentHash/consistentHash.go
@@ -0,0 +1,56 @@
+package consistentHash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int //sorted
+	hashMap  map[int]string
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			//将下标i也转化为字符串，加入hash值的计算中，保证虚拟结点的唯一性
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	//排序
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+
+	// Binary search for appropriate replica.
+	idx := sort.SearchInts(m.keys, hash)
+
+	//当hash值大于所有m.keys[i]时，idx = len(m.keys)，因此要取余
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
